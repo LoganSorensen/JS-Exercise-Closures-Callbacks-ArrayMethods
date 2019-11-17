84098a974f8ed85e15f81302bf812fd71b41994d
Logan Sorensen <loganmsorensen@gmail.com>
2019-11-16 20:07:23 -0800

    passed firstNamesAllCaps

--- index.js
+++ index.js
@@ -200,8 +200,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const capsNames = runners.map(function(element){
+    return `${element.first_name.toUpperCase()}`;
+  })
+
+  return capsNames;
 }
 
 /**
